7642fc56f00dfb44d6d4a8bcf6615487b0738d57
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-05-20 17:25:01 +0200

    false was a string -> boolean

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -29,7 +29,7 @@ export async function getStaticPaths() {
 
   return {
     paths: movies.map((movie) => ({ params: { id: movie.id.toString() } })),
-    fallback: "false",
+    fallback: false,
   };
 }
 
